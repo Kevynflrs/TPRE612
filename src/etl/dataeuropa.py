22bd9a610a58f4b254d9c82e055808502743c750
lefarmer01600 <t.clemenceau@outlook.fr>
2026-02-27 13:51:52 +0100

    feat: Add ETL process for Eurostat and Night Train data
    - Implemented gestion_etl.py for managing database connections and loading datasets.
    - Added DatabaseManager class for handling UPSERT operations in PostgreSQL.
    - Created night_train_data.py to process various JSON files related to night train data.
    - Introduced functions for loading and cleaning data from JSON files into Pandas DataFrames.
    - Established logging for better tracking of data processing and database operations.

--- src/etl/dataeuropa.py
+++ src/etl/dataeuropa.py
@@ -41,24 +41,41 @@ def etl_process_europa_portal(dataset_id):
 
 # 2. EXECUTION DU PROCESSUS SUR PLUSIEURS IDs
 # Ajoute ici tous les IDs que tu souhaites traiter
+# liste_datasets = [
+#     "fdfc3d62-86dd-4104-853f-2c89e676561f",
+#     "gxrjq6yzvzzjtrx2cv9vsg",
+#     "qg5eeq1bhoqolcow3fojra"
+# ]
 liste_datasets = [
-    "fdfc3d62-86dd-4104-853f-2c89e676561f",
-    "gxrjq6yzvzzjtrx2cv9vsg",
-    "qg5eeq1bhoqolcow3fojra"
+    {
+        "id": "fdfc3d62-86dd-4104-853f-2c89e676561f",
+        "name": "trajet_train_europe"
+    },
+    {
+        "id": "gxrjq6yzvzzjtrx2cv9vsg",
+        "name": "source_energie_train_perf"
+    },
+    {
+        "id": "qg5eeq1bhoqolcow3fojra",
+        "name": "nombre_passagers_train"
+    }
 ]
 
-resultats_datasets = {}
 
-print("=== DEMARRAGE DE L'ETL ===")
 
-for ds_id in liste_datasets:
-    print(f"\nTraitement de la source : {ds_id}...")
-    df_temp = etl_process_europa_portal(ds_id)
-    
-    if df_temp is not None:
-        resultats_datasets[ds_id] = df_temp
-        print(f"-> Source {ds_id} enregistrée.")
-    else:
-        print(f"-> Source {ds_id} ignorée suite à une erreur.")
+def get_data_europa():
+    resultats_datasets = {}
 
-print("\n=== TOUTES LES SOURCES ONT ÉTÉ TRAITÉES ===")
+    for ds in liste_datasets:
+        ds_id = ds["id"]
+        ds_name = ds["name"]
+        print(f"\nTraitement de la source : {ds_name} (ID: {ds_id})...")
+        df_temp = etl_process_europa_portal(ds_id)
+        
+        if df_temp is not None:
+            resultats_datasets[ds_name] = df_temp
+            print(f"-> Source {ds_name} enregistrée.")
+        else:
+            print(f"-> Source {ds_name} ignorée suite à une erreur.")
+    return resultats_datasets
+
