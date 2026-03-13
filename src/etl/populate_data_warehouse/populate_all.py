ec7e4416bd31d6b3168266eaabe69da3f4ebdcd2
lefarmer01600 <t.clemenceau@outlook.fr>
2026-03-13 18:39:25 +0100

    feat: update import statements in populate_all.py for path management

--- src/etl/populate_data_warehouse/populate_all.py
+++ src/etl/populate_data_warehouse/populate_all.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent))
+
 from database import DatabaseManager, DB_CONFIG
 import pandas as pd
 from dim_date import populate_dim_date
